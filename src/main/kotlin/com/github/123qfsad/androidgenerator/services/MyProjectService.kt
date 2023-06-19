becad1dc0db457369b3b2db2db75189608a822d4
GitHub Action <action@github.com>
2023-06-19 07:48:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/123qfsad/androidgenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/123qfsad/androidgenerator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.123qfsad.androidgenerator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.123qfsad.androidgenerator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
